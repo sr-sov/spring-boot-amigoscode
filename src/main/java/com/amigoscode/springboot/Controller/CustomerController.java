9b5d19a43ab6af4da4852d48b52e3a98ff53afe0
Joshua David Sta Rita <jdcstarita@gmail.com>
2023-03-20 13:11:11 +0800

    Customer POST API

--- src/main/java/com/amigoscode/springboot/Controller/CustomerController.java
+++ src/main/java/com/amigoscode/springboot/Controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.amigoscode.springboot.Controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +17,27 @@ public class CustomerController {
 	
 	private final CustomerRepository customers;
 
-	public CustomerController(CustomerRepository customers) {
-		this.customers = customers;
+	public CustomerController(CustomerRepository customersRepo) {
+		customers = customersRepo;
 	}
 
 	@GetMapping
 	public List<Customer> getCustomers() {
 		return customers.findAll();
+	}
+	
+	record NewCustomerRequest(
+			String name,
+			String email,
+			Integer age
+			){}
+	
+	@PostMapping
+	public void addCustomer(@RequestBody NewCustomerRequest request) {
+		Customer customer = new Customer();
+		customer.setName(request.name());
+		customer.setEmail(request.email());
+		customer.setAge(request.age());
+		customers.save(customer);
 	}
 }
